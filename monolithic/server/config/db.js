5fe4d3b5a53be416d84dd2d06d55d7b2d3bb598c
Samik Malhotra <samikmalhotra66@gmail.com>
2021-12-03 22:21:52 +0530

    added env

--- monolithic/server/config/db.js
+++ monolithic/server/config/db.js
@@ -5,10 +5,10 @@ dotenv.config()
 const connectDB = async () => {
   console.log(process.env.MONGO_URI)
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect("mongodb+srv://user:user@cluster0.877ii.mongodb.net/project-zeus?retryWrites=true&w=majority", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true,
+      // useCreateIndex: true,
     })
 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
--- monolithic/server/server.js
+++ monolithic/server/server.js
@@ -1,33 +1,33 @@
 import path from 'path'
 import express from 'express'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+// import { notFound, errorHandler } from './middlewares/errorMiddlewares'
 import connectDB from './config/db.js'
 import morgan from 'morgan'
-
-import userRoutes from './routes/userRoutes.js'
-
+import dotenv from 'dotenv'
 dotenv.config()
+// import userRoutes from './routes/userRoutes.js'
+
 
 connectDB()
 
 const app = express()
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
-  }
+// if (process.env.NODE_ENV === 'development') {
+//     app.use(morgan('dev'))
+//   }
 
   app.use(express.json())
 
 //   app.use('/api/products', productRoutes)
-  app.use('/api/users', userRoutes)
+//   app.use('/api/users', userRoutes)
 //   app.use('/api/orders', orderRoutes)
 //   app.use('/api/upload', uploadRoutes)
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+    // const __dirname = path.resolve()
+    // app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-app.use(notFound)
-app.use(errorHandler)
+// app.use(notFound)
+// app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
