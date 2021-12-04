ea7d06838bdcb11afce88b3518e223860d142cc7
Samik Malhotra <samikmalhotra66@gmail.com>
2021-12-04 07:44:03 +0530

    added productModel

---
+++ monolithic/server/models/productModel.js
@@ -0,0 +1,43 @@
+import mongoose, { Mongoose } from 'mongoose'
+
+const productSchema = mongoose.Schema({ 
+    shop: {
+        type: Mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Shop'
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0,
+      },
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+},
+    {
+        timestamps: true
+    }
+)
